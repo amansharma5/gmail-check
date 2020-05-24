da16ea6abdced54ec8c9068c61113c04aef684b8
Aman sharma <amansharma5@hotmail.com>
2020-05-24 18:23:52 +0530

    email checker updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gmail-check
-Check gmail existence!!!
+Check gmail existence.
+
+This gmail-check library provides the way to know the existence of gmail mailbox written in server-side JavaScript.
+
+###Installation
+Install the package with:
+```
+npm install gmail-check
+```
+### Include 
+```javascript
+var gmailCheck = require('gmail-check');
+```
+
+### Example Using async/await
+```javascript
+
+let result = await gmailCheck("abc@xyz.com"); // gmail address to test
+console.log(result)  ///// status : "success" (if exists) || status : "failed" (if doesn't exists)
+-- Do Something --
+```
+
+---
+### Submit Queries
+You can e-mail an issue/query at amansharma5@hotmail.com
--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const net = require('net')
 
-let email = "<theamansharma5@gmail.com>"
-const commands = [
-  'HELO gmail\r\n',
-  'MAIL FROM: <theamansharma5@gmail.com>\r\n',
-  `RCPT TO: ${email}\r\n`,
-  'DATA\r\n',
-  `FROM: <theamansharma5@gmail.com>\r\nTO: ${email}\r\nSUBJECT: Testing\r\nThis is cool!\r\n.\r\n`,
-  'QUIT\r\n'
-]
-
-let i = 0
-
-const socket = net.createConnection(25, 'gmail-smtp-in.l.google.com', () => console.log('Connected to gmail SMTP server.'))
-
-socket.on('data', buff => {
-  const result = buff.toString()
-  if(result.includes('550-5.1.1')){
-    console.log("Email Doesn't Exists");
-    socket.destroy()
-  }else if (result.includes('221 2.0.0 closing connection')) {
-    socket.end(()=>{console.log("End")})
-  } else if (i < commands.length) {
-    console.log("Email exists");
-    socket.write(commands[++i])
-  } else {
-    console.log('Ran out of commands but did not receive 221 response from SMTP server')
-    socket.destroy()
-  }
-})
-
-socket.write(commands[i])
-
+let status = {
+    failed : {status : "failed",message : "Email doesn't exists"},
+    success:{status : "success",message: "Email exists"}
+}
+module.exports = async(payloadData)=>{
+  return await new Promise((resolve, reject)=>{
+    try {
+          const commands = [
+            'HELO gmail\r\n',
+            'MAIL FROM: <emailchecknpm@gmail.com>\r\n',
+            `RCPT TO: <${payloadData}>\r\n`,
+            'DATA\r\n',
+            `FROM: <emailchecknpm@gmail.com>\r\nTO: <${payloadData}>\r\nSUBJECT: Email Testing\r\nTesting the existence of an email address!\r\n.\r\n`,
+            'QUIT\r\n'
+            ]
+          let i = 0
+          let count = 0
+          const socket = net.createConnection(25, 'gmail-smtp-in.l.google.com');
+            socket.on('data', buff => {
+            const result = buff.toString()
+              if(result.includes('550-5.1.1')){
+                  resolve(status.failed);
+                  socket.destroy();
+              }else if(result.includes('221 2.0.0 closing connection')) {
+                  socket.end();
+              }else if(i < commands.length) {
+                  socket.write(commands[++i]);
+                  count += 1;
+                    if(count > 3){resolve(status.success); socket.destroy()}
+              }else{
+                  resolve('Cannot Connect to server');
+                  socket.destroy()
+              }
+            })
+          socket.write(commands[i])
+    } catch (error) {
+        reject(error);
+    }        
+  })
+}
